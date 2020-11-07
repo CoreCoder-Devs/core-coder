2fce5c3bed35e51573cab34225be75661cf578bd
skybird23333 <2603003199a@gmail.com>
2020-11-07 15:21:40 +0800

    Feat: Hide menu & set min window size

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const Electron = require('electron');
 
+
 Electron.app.whenReady().then(()=>{
     var win = new Electron.BrowserWindow({
         title: "CoreCoder by Hanprogramer",
         webPreferences : {nodeIntegration: true, webviewTag: true, allowRunningInsecureContent: true, nodeIntegrationInSubFrames: true, enableRemoteModule:true},
-        icon: __dirname + "/src/resources/icon.ico"
+        icon: __dirname + "/src/resources/icon.ico",
+        minWidth: 800,
+        minHeight: 600,
     });
+    win.setMenuBarVisibility(false)
     win.loadFile("src\\project_manager.html");
 });
