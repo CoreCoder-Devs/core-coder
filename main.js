3cdb19950949cc7a81df68ad3050b47799f98877
skybird23333 <2603003199a@gmail.com>
2020-11-07 15:27:33 +0800

    Further improvement on window size

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ const Electron = require('electron');
 
 Electron.app.whenReady().then(()=>{
     var win = new Electron.BrowserWindow({
+        width: 1000,
+        height: 750,
         title: "CoreCoder by Hanprogramer",
         webPreferences : {nodeIntegration: true, webviewTag: true, allowRunningInsecureContent: true, nodeIntegrationInSubFrames: true, enableRemoteModule:true},
         icon: __dirname + "/src/resources/icon.ico",
-        minWidth: 800,
-        minHeight: 600,
+        minWidth: 700,
+        minHeight: 400,
     });
     win.setMenuBarVisibility(false)
     win.loadFile("src\\project_manager.html");
