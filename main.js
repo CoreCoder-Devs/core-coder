ff1ac2807ed185d0f595782d9a71400d4eb83fc6
Hanprogramer <30620423+Hanprogramer@users.noreply.github.com>
2020-11-08 20:58:18 +0700

    Removed some useless codes

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 const Electron = require('electron');
-const { app, autoUpdater, dialog } = require('electron');
-
 const RPC = require('discord-rpc')
- 
 const client = new RPC.Client({ transport: 'ipc' });
 
 client.on('ready', () => {
@@ -19,13 +16,6 @@ client.login({clientId: '774605779952468022'}).catch(e=> {
   console.log(e)
 })
 
-// Checks for update
-require('update-electron-app')({
-    repo: 'CoreCoder-Devs/CoreCoder-One',
-    updateInterval: '1 hour'
-  })
-
-
 Electron.app.whenReady().then(()=>{
     var win = new Electron.BrowserWindow({
         width: 1000,
@@ -38,21 +28,4 @@ Electron.app.whenReady().then(()=>{
     });
     win.setMenuBarVisibility(false)
     win.loadFile("src\\project_manager.html");
-    // win.once('ready-to-show', () => {
-    //     autoUpdater.checkForUpdatesAndNotify();
-    // });
-    // autoUpdater.on('update-available', () => {
-    //     win.webContents.send('update_available');
-    // });
-    // autoUpdater.on('update-downloaded', () => {
-    //     win.webContents.send('update_downloaded');
-    // });
-    
 });
-Electron.ipcMain.on('restart_app', () => {
-    // autoUpdater.quitAndInstall();
-  });
-Electron.ipcMain.on('app_version', (event) => {
-    event.sender.send('app_version', { version: app.getVersion() });
-});
-
